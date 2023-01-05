eea7a1a7d68af6b1945764052fa5fe29ecad77e4
Heulween19 <anayang1907@gmail.com>
2023-01-05 09:26:05 +0700

    Remove ignored files

---
+++ Tr_Excercise/EventBaby/Parent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBaby
+{
+    public delegate void EventNewBaby(object sender, NewBabyEventArgs e);
+
+    
+    public class Parent
+    {
+        public event EventNewBaby NewBaby;
+        protected virtual void OnNewBAby(NewBabyEventArgs e)
+        {
+            if (NewBaby != null)
+                NewBaby(this, e);
+        }
+
+        public void NotifyNewBaby(string Name,string Gender)
+        {
+            NewBabyEventArgs e = new NewBabyEventArgs(Name,Gender);
+            Console.WriteLine($"Baby Info {e.Name} {e.Gender}");
+            OnNewBAby(e);
+        }
+    }
+    
+    public class NewBabyEventArgs
+    {
+        public string Name;
+        public string Gender;
+
+        public NewBabyEventArgs(string name,string gender)
+        {
+            Name = name;
+            Gender = gender;
+        }
+    }
+
+}
---
+++ Tr_Excercise/EventBaby/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EventBaby
+{
+    public class Program
+    {
+       public static void CallFriend(object sender, NewBabyEventArgs e)
+        {
+            Console.WriteLine("Send a message...");
+        }
+        static void Main(string[] args)
+        {
+            Parent p = new Parent();
+            p.NewBaby += CallFriend;
+            p.NotifyNewBaby("Baby", "Male");
+
+            Console.ReadKey();
+
+        }
+    }
+}
